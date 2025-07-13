68329eb7b0f0aaf80b1db4cd7732954badc73b35
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-07-13 21:27:13 +0000

    Reveal animation codebase

---
+++ revealAnimation/projects.js
@@ -0,0 +1,57 @@
+export const projectsData = [
+    {
+        name: "Lunar Eclipse",
+        director: "Amelia Crawfor",
+        location: "Toronto, ON"
+    },
+    {
+        name: "Solar Rising",
+        director: "James Holloway",
+        location: "Vancouver, BC"
+    },
+    {
+        name: "Echoes of the Past",
+        director: "Nina Martinez",
+        location: "Montreal, QC"
+    },
+    {
+        name: "Crimson Horizon",
+        director: "Liam Chen",
+        location: "Calgary, AB"
+    },
+    {
+        name: "Silent Winter",
+        director: "Olivia Brooks",
+        location: "Ottawa, ON"
+    },
+    {
+        name: "Midnight Pulse",
+        director: "Kai Thompson",
+        location: "Winnipeg, MB"
+    },
+    {
+        name: "Winds of Change",
+        director: "Sophia Patel",
+        location: "Halifax, NS"
+    },
+    {
+        name: "The Velvet Storm",
+        director: "Noah Bennett",
+        location: "Edmonton, AB"
+    },
+    {
+        name: "Fragments",
+        director: "Chloe Dubois",
+        location: "Quebec City, QC"
+    },
+    {
+        name: "The Final Script",
+        director: "Ethan Walker",
+        location: "Regina, SK"
+    },
+    {
+        name: "Beyond the Frame",
+        director: "Zara Khan",
+        location: "St. John's, NL"
+    }
+]
